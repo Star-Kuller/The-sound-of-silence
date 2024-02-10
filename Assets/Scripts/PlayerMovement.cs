6f57836e4c5c0a6df58c4998b87155018a57ae23
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2024-02-10 21:53:08 +0300

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f; // скорость передвижения
+    private Rigidbody2D _rb; // компонент Rigidbody игрока
+
+    private void Start()
+    {
+        // Получаем компонент Rigidbody
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Получаем ввод от пользователя
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Создаем вектор движения
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        // Применяем движение к Rigidbody
+        _rb.velocity = movement * speed;
+    }
+}
