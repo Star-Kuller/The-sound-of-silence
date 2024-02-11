11a869c5827181427788bdcb2b4f17977ecd6f0f
Kirill <kirill.k@ugo.company>
2024-02-11 15:49:37 +0300

    character animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,18 +7,23 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f; // скорость передвижения
     private Rigidbody2D _rb; // компонент Rigidbody игрока
+    private Animator _animator; // компонент Animator игрока
 
     private void Start()
     {
         // Получаем компонент Rigidbody
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         // Получаем ввод от пользователя
         var moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _animator.SetInteger("MoveHorizontal",  (int)Math.Round(moveHorizontal));
         var moveVertical = Input.GetAxisRaw("Vertical") * 0.6;
+        _animator.SetInteger("MoveVertical", (int)Math.Round(moveVertical));
+        Debug.Log($"h = {moveVertical} v = {moveHorizontal}");
 
         // Создаем вектор движения
         var movement = new Vector2(moveHorizontal, (float)moveVertical).normalized;
